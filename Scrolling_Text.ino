a83702580ff6e9bae8241ca9a586b50f7431b8e5
Liam-07J <94325296+Liam-07J@users.noreply.github.com>
2022-03-29 20:40:11 +0100

    Initial commit

---
+++ Scrolling_Text.ino
@@ -0,0 +1,19 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16, 2);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.begin(16, 2);
+  char array[] = {"Hello"};
+  int size = sizeof array / sizeof array[0];
+  int speed = 500;
+  for (int i = 0; i < size; i++) {
+    delay(speed);
+    lcd.print( array[i]);
+  }
+}
